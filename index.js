b2561dd4a606e8053eee00464933c0244c58058e
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-03-20 09:47:23 +0600

    learning javascript

---
+++ index.js
@@ -0,0 +1,44 @@
+// var num1 = "55";
+// var num1Convartet = parseFloat(num1);
+// var num2 = 99;
+// var total = num1Convartet + num2;
+// console.log(total);
+
+//==========number to string
+
+// var number3 = 143;
+// var number3Convartet = "" + number3;
+// console.log(number3Convartet);
+
+//==========number operator
+
+// var number1 = 60;
+// var number2 = 40;
+// var jog = number1 + number2;
+// var biog = number1 - number2;
+// var gun = number1 * number2;
+// var vag = number1 / number2;
+// var vagsesh = number1 % number2;
+// console.log(vag);
+
+//==========predefined function of javascript
+number1 = 20;
+number2 = 450;
+
+
+//==========condition
+
+var a = 5;
+var b = 4;
+if(a < b){
+    console.log("A is lesthan B");
+} else if (a > b) {
+    console.log("A is geater than B");
+} else{
+    console.log("Both are equal.");
+}
+
+//==========switch statement 
+
+//==========javascript array
+
