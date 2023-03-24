747c15d855c0c2ed6bf7f4132513585e4e6b998d
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-03-24 13:51:11 +0600

    ja object

---
+++ app.js
@@ -0,0 +1,13 @@
+//====================JS OBJECT START====================//
+myInfo = {
+    name : "Nayeem Khan",
+    age : 32,
+    phone : "Nokia",
+    salary : 5000
+}
+
+// console.log(myInfo.age);
+var singleData = myInfo.phone;
+
+console.log(singleData);
+//====================JS OBJECT ENDED====================//
