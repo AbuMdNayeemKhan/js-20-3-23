9792935c09e1abad7e2fcf77a91ec44070952e94
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-03-28 23:30:32 +0600

    learning javascript

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 //====================JS OBJECT START====================//
-myInfo = {
-    name : "Nayeem Khan",
-    age : 32,
-    phone : "Nokia",
-    salary : 5000
-}
+// myInfo = {
+//     name : "Nayeem Khan",
+//     age : 32,
+//     phone : "Nokia",
+//     salary : 5000
+// }
 
 // console.log(myInfo.age);
-var singleData = myInfo.phone;
+// var singleData = myInfo.phone;
 
-console.log(singleData);
-//====================JS OBJECT ENDED====================//
+// console.log(singleData);
+
+function MyObj(name, age, phone, salary){
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.salary = salary;
+    this.showMe = function(){
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.phone);
+        console.log(this.salary);
+    }
+}
+
+var ofNayeem = new MyObj("Nayeem", 22, 01619981998, 5000);
+
+console.log(ofNayeem.showMe());
+
+//====================JS OBJECT ENDED====================//  
